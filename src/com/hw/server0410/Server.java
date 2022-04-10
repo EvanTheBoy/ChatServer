6810e144dcc65569656922dea686666b492132f4
Evan <761356554@qq.com>
2022-04-10 22:13:39 +0800

    Updated some changes

--- src/com/hw/server0410/Server.java
+++ src/com/hw/server0410/Server.java
@@ -3,18 +3,22 @@ package com.hw.server0410;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Server {
     private HashMap<Socket, Integer> socketList = new HashMap<>();
+    private List<Integer> idList = new ArrayList<>();
     public void createServer(int port) {
         int i = 1;
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
                 Socket client = serverSocket.accept();
+                idList.add(i);
                 socketList.put(client, i++);
-                ServerThread st = new ServerThread(client, socketList);
+                ServerThread st = new ServerThread(client, socketList, idList);
                 Thread t1 = new Thread(st);
                 t1.start();
             }
--- src/com/hw/server0410/ServerThread.java
+++ src/com/hw/server0410/ServerThread.java
@@ -3,18 +3,30 @@ package com.hw.server0410;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ServerThread implements Runnable, MsgType {
     private Socket s;
     private HashMap<Socket, Integer> socketList;
-    public ServerThread(Socket s, HashMap<Socket, Integer> socketList) {
+    private List<Integer> idList;
+    public ServerThread(Socket s, HashMap<Socket, Integer> socketList, List<Integer> idList) {
         this.s = s;
         this.socketList = socketList;
+        this.idList = idList;
     }
-    //把当前在线客户端个数群发给所有在的用户
-    public void sendUser(OutputStream os){
 
+    //把当前在线客户端个数群发给所有在的用户
+    public void sendUser(OutputStream os) throws Exception{
+        //在线好友个数
+        int len = idList.size();
+        os.write(len);
+        for (int i = 0; i < len; ++i) {
+            String userInfo = "死党" + idList.get(i) + "加入聊天!";
+            os.write(userInfo.getBytes());
+        }
+        os.flush();
     }
 
     @Override
@@ -27,14 +39,7 @@ public class ServerThread implements Runnable, MsgType {
                 System.out.println("用户数量:" + socketList.keySet().size());
                 output = socket.getOutputStream();
                 output.write(USER);
-                int len = socketList.size();
-                output.write(len);
-                for(int i=0;i<len;i++) {
-                    System.out.println("ServerThread发送用户上线消息协议:" + USER);
-                    String userInfo = "死党" + socketList.get(socket) + "加入聊天!";
-                    output.write(userInfo.getBytes());
-                    output.flush();
-                }
+                sendUser(output);
                 System.out.println("用户上线提示消息发送完毕，未确认是否收到...");
             }
 
